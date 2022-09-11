71e3a66ea8bd9448d1131d8b3ea3b1ad8c47d575
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-09-12 00:45:25 +0200

    Creo que conseguido Imagen y Content Crear y Actualizar

--- Pablo/app/Controllers/Http/ImagesController.ts
+++ Pablo/app/Controllers/Http/ImagesController.ts
@@ -1,7 +1,8 @@
 import { ResponsiveAttachment } from '@ioc:Adonis/Addons/ResponsiveAttachment'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Image from 'App/Models/Image'
-
+import CreateImageValidator from 'App/Validators/CreateImageValidator'
+import UpdateImageValidator from 'App/Validators/UpdateImageValidator'
 export default class ImagesController {
   public async index({ response }: HttpContextContract) {
     const images = await Image.query()
@@ -9,23 +10,37 @@ export default class ImagesController {
   }
 
   public async store({ request, response }: HttpContextContract) {
-    try {
-      const imageobjeto = new Image()
-      const image = request.file('image_xl_md')
-      const imagedos = request.file('image_lg_sm')
-      imageobjeto.imageXlMd = image ? await ResponsiveAttachment.fromFile(image) : null
-      imageobjeto.imageLgSm = imagedos ? await ResponsiveAttachment.fromFile(imagedos) : null
-      imageobjeto.contentimageid = request.input('contentimageid')
-      const movies = await Image.create(imageobjeto)
-      return response.ok(movies)
-    } catch (error) {
-      response.badRequest(error.messages)
-    }
+    const data = await request.validate(CreateImageValidator)
+    const imageobjeto = new Image()
+    const imageuno = request.file('image_xl_md')
+    const imagedos = request.file('image_lg_sm')
+    imageobjeto.imageXlMd = imageuno ? await ResponsiveAttachment.fromFile(imageuno) : null
+    imageobjeto.imageLgSm = imagedos ? await ResponsiveAttachment.fromFile(imagedos) : null
+    imageobjeto.contentimageid = data.contentimageid
+    const images = await Image.create(imageobjeto)
+    return response.ok(images)
   }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ response, params }: HttpContextContract) {
+    const idmanual = params.id
+    console.log('idmanual ', idmanual)
+    const data = await Image.query().where('id', idmanual)
+    return response.ok(data)
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, response, params }: HttpContextContract) {
+    await request.validate(UpdateImageValidator)
+    const idmanual = params.id
+    const image = await Image.firstOrFail()
+    const imageXlMd = request.file('image_xl_md')
+    const imageLgSm = request.file('image_lg_sm')
+    image.imageXlMd = imageXlMd ? await ResponsiveAttachment.fromFile(imageXlMd) : null
+    image.imageLgSm = imageLgSm ? await ResponsiveAttachment.fromFile(imageLgSm) : null
+    // Old file will be removed from the disk as well.
+    await image.save()
+    const query = await Image.query().where('id', idmanual)
+    return response.ok(query)
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
